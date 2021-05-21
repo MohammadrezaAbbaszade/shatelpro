e7de1cfe67071d5d965d6a0b36255d5ede93f2f8
mohammadreza <abbaszadeh@gmail.com>
2021-05-21 20:39:50 +0430

    changed something

--- app/src/main/java/com/example/shatelpro/MainActivity.java
+++ app/src/main/java/com/example/shatelpro/MainActivity.java
@@ -2,27 +2,29 @@ package com.example.shatelpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-        editText.setOnClickListener(new View.OnClickListener() {
+        signUpButton=findViewById(R.id.sign_up_btn);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ScaleAnimation animate = new ScaleAnimation(1, 2, 1, 1, Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0);
-                animate.setDuration(700);
-                animate.setFillAfter(true);
-                editText.startAnimation(animate);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.my.shatel.ir/RegisterAdslUser"));
+                startActivity(browserIntent);
             }
         });
 
